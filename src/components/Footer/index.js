9a68bde406877f8cc601b9241327738b566e757c
Daniela Schalldach <sulikova.daniela@gmail.com>
2023-05-08 11:28:42 +0200

    transfer of CSS and HTML

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import './style.css';
+
+export const Footer =()=>{
+const element=document.createElement("footer");
+element.innerHTML=`
+<footer>
+        <div class="container">
+          <div class="footer__content">
+            Café Lóra je tréningový projekt v rámci Czechitas kurzu JavaScript 2
+          </div>
+        </div>
+</footer>
+`;
+};
