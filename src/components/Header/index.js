a5926439a5cfbdaace4a84b986f9b36afad8a7e0
Daniela Schalldach <sulikova.daniela@gmail.com>
2023-05-08 13:45:25 +0200

    header and footer done...maybe

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,23 +1,57 @@
 import './style.css';
 
-export const Header =()=>{
+export const Header =(props)=>{
+    const {menuBurger}=props
+    
 const element=document.createElement("header");
-element.innerHTML=`
-<header>
-        <div class="header__content container">
-          <div class="site-logo"></div>
-  
-          <div class="navigation">
-            <button class="nav-btn"></button>
-            <nav class="rollout-nav nav-closed">
-              <a href="#home">domů</a>
-              <a href="#menu">menu</a>
-              <a href="#gallery">galerie</a>
-              <a href="#contact">kontakt</a>
-            </nav>
-          </div>
-          </div>
-</header>
-`;
 
+// tato podmínka dělá to samé jako ${menuBurger} v .innerHTML, ale to druhé je rychlejší a lépe zakomponovatelné (asi)
+// if(menuBurger){
+//     `<div class="navigation">
+//             <button class="nav-btn"></button>
+//             <nav class="rollout-nav nav-closed">
+//               <a href="#home">domů</a>
+//               <a href="#menu">menu</a>
+//               <a href="#gallery">galerie</a>
+//               <a href="#contact">kontakt</a>
+//             </nav>
+//           </div>
+//           `;
+
+// }else {
+//     `<nav class="inline-nav">
+//           <a href="/">Hlavní stránka</a>
+//         </nav>
+//     `;
+// }
+
+
+element.innerHTML=`
+<div class="header__content container">
+      <div class="site-logo"></div>
+      ${showMenu ? `
+        <div class="navigation">
+          <button class="nav-btn"></button>
+          <nav class="rollout-nav nav-closed">
+            <a href="#home">domů</a>
+            <a href="#menu">menu</a>
+            <a href="#gallery">galerie</a>
+            <a href="#contact">kontakt</a>
+          </nav>
+        </div>
+      ` : `
+        <nav class="inline-nav">
+          <a href="/">Hlavní stránka</a>
+        </nav>
+      `}
+    </div>
+`;
+const navBtnElm=element.querySelector(".nav-btn");
+if (navBtnElm !==null) {
+    navBtnElm.addEventListener("click", () => {
+    element.querySelector("nav").classList.toggle("nav-closed")
+    });  
+}
+
+return element;
 };
